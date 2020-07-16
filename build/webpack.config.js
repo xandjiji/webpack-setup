29eddca9696e47fdb7c0c7add34e96528739f415
xand <xandjiji@gmail.com>
2020-07-16 11:23:55 -0300

    Update webpack.config.js

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -55,7 +55,7 @@ module.exports = {
                 ]
             },
             {
-                test: /\.(jpg|png|gif)$/,
+                test: /\.(jpg|png|gif|svg|ttf|woff|woff2)$/,
                 use: [
                     {
                         loader: "file-loader",
