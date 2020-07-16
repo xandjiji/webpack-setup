59f94a362ec582740170e3d35f558d64e3c5d194
xand <xandjiji@gmail.com>
2020-07-16 14:20:15 -0300

    Update webpack.config.js

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin')
 const autoprefixer = require('autoprefixer');
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
@@ -10,7 +9,7 @@ const isDevelopment = process.env.NODE_ENV !== 'production';
 module.exports = {
     entry: {
         bundle: './src/app.js'
-    } ,
+    },
     output: {
         path: path.resolve(__dirname, '../dist')
     },
@@ -61,58 +60,55 @@ module.exports = {
                         loader: "file-loader",
                         options: {
                             name: '[name].[ext]',
-                            outputPath: 'assets/',
                             useRelativePath: true,
                         }
                     },
                     {
                         loader: 'image-webpack-loader',
                         options: {
-                          mozjpeg: {
-                            progressive: true,
-                            quality: 65
-                          },
-                          optipng: {
-                            enabled: true,
-                          },
-                          pngquant: {
-                            quality: '65-90',
-                            speed: 4
-                          },
-                          gifsicle: {
-                            interlaced: false,
-                          },
-                          webp: {
-                            quality: 75
-                          }
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            },
+                            optipng: {
+                                enabled: true,
+                            },
+                            pngquant: {
+                                quality: '65-90',
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            webp: {
+                                quality: 75
+                            }
                         }
                     }
                 ]
             }
-        ] 
+        ]
     },
     plugins: [
-        /** Since Webpack 4 */
         new webpack.LoaderOptionsPlugin({
             options: {
-              handlebarsLoader: {}
+                handlebarsLoader: {}
             }
-          }),
-          new MiniCssExtractPlugin({
+        }),
+        new MiniCssExtractPlugin({
             filename: "[name]-styles.css",
             chunkFilename: "[id].css"
-          }),  
-         
+        }),
+
         new HtmlWebpackPlugin({
             template: './src/index.handlebars',
             minify: !isDevelopment && {
                 html5: true,
                 collapseWhitespace: true,
                 caseSensitive: true,
-                removeComments: true,
-                removeEmptyElements: true
+                removeComments: true
             },
 
-          })
-      ]
-  };
+        })
+    ]
+};
